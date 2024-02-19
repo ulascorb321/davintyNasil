60d7e33983194143913683c385c4b55fef762262
ulascorb_ <107434656+ulascorb321@users.noreply.github.com>
2024-02-19 10:20:56 +0300

    Add files via upload

--- d.js
+++ d.js
@@ -1,14 +1,19 @@
 const sliders = [...document.querySelectorAll(".slider__container")];
 const sliderControlPrev = [...document.querySelectorAll(".slider__control.prev")];
 const sliderControlNext = [...document.querySelectorAll(".slider__control.next")];
+let img1 = document.getElementById('img1');
+let img2 = document.getElementById('img2');
+let img3 = document.getElementById('img3');
+let img4 = document.getElementById('img4');
+var isSecondImage = false;
 
 sliders.forEach((slider, i) => {
   let isDragStart = false,
-      isDragging = false,
-      isSlide = false,
-      prevPageX,
-      prevScrollLeft,
-      positionDiff;
+    isDragging = false,
+    isSlide = false,
+    prevPageX,
+    prevScrollLeft,
+    positionDiff;
 
   const sliderItem = slider.querySelector(".slider__item");
   var isMultislide = (slider.dataset.multislide === 'true');
@@ -18,24 +23,24 @@ sliders.forEach((slider, i) => {
     isSlide = true;
     let slideWidth = isMultislide ? slider.clientWidth : sliderItem.clientWidth;
     slider.scrollLeft += -slideWidth;
-    setTimeout(function(){ isSlide = false; }, 700);
+    setTimeout(function () { isSlide = false; }, 700);
   });
 
   sliderControlNext[i].addEventListener('click', () => {
     if (isSlide) return;
     isSlide = true;
-    let slideWidth = isMultislide ? slider.clientWidth : sliderItem.clientWidth ;
+    let slideWidth = isMultislide ? slider.clientWidth : sliderItem.clientWidth;
     slider.scrollLeft += slideWidth;
-    setTimeout(function(){ isSlide = false; }, 700);
+    setTimeout(function () { isSlide = false; }, 700);
   });
 
   function autoSlide() {
-    if(slider.scrollLeft - (slider.scrollWidth - slider.clientWidth) > -1 || slider.scrollLeft <= 0) return;
+    if (slider.scrollLeft - (slider.scrollWidth - slider.clientWidth) > -1 || slider.scrollLeft <= 0) return;
     positionDiff = Math.abs(positionDiff);
     let slideWidth = isMultislide ? slider.clientWidth : sliderItem.clientWidth;
     let valDifference = slideWidth - positionDiff;
-    if(slider.scrollLeft > prevScrollLeft) {
-        return slider.scrollLeft += positionDiff > slideWidth / 5 ? valDifference : -positionDiff;
+    if (slider.scrollLeft > prevScrollLeft) {
+      return slider.scrollLeft += positionDiff > slideWidth / 5 ? valDifference : -positionDiff;
     }
     slider.scrollLeft -= positionDiff > slideWidth / 5 ? valDifference : -positionDiff;
   }
@@ -46,11 +51,11 @@ sliders.forEach((slider, i) => {
     isDragStart = true;
     prevPageX = e.pageX || e.touches[0].pageX;
     prevScrollLeft = slider.scrollLeft;
-    setTimeout(function(){ isSlide = false; }, 700);
+    setTimeout(function () { isSlide = false; }, 700);
   }
 
   function dragging(e) {
-    if(!isDragStart) return;
+    if (!isDragStart) return;
     e.preventDefault();
     isDragging = true;
     slider.classList.add("dragging");
@@ -61,7 +66,7 @@ sliders.forEach((slider, i) => {
   function dragStop() {
     isDragStart = false;
     slider.classList.remove("dragging");
-    if(!isDragging) return;
+    if (!isDragging) return;
     isDragging = false;
     autoSlide();
   }
@@ -75,8 +80,66 @@ sliders.forEach((slider, i) => {
   slider.addEventListener("touchend", dragStop);
   slider.addEventListener("mouseleave", dragStop);
 });
-
+let i = 0;
 var quadimages = document.querySelectorAll("#quad figure");
-for(i=0; i<quadimages.length; i++) {
-  quadimages[i].addEventListener('click', function(){ this.classList.toggle("expanded"); quad.classList.toggle("full") }); 
+for (i = 0; i < quadimages.length; i++) {
+  quadimages[i].addEventListener('click', function () { this.classList.toggle("expanded"); quad.classList.toggle("full") });
+}
+
+img1.onclick = function () {
+  img1.style.opacity = '5%';
+  setTimeout(function () {
+    if (isSecondImage) {
+      img1.src = 'Yeşil İllüstrasyon Romantik Sevimli Düğün Davetiyesi.png';
+    }
+    else {
+      img1.src = 'f.png';
+    }
+    isSecondImage = !isSecondImage;
+    img1.style.opacity = 1;
+  }, 800);
+}
+
+img2.onclick = function () {
+  img2.style.opacity = '5%';
+  setTimeout(function () {
+    if (isSecondImage) {
+      img2.src = 'dd.png';
+    }
+    else {
+      img2.src = 'f.png';
+    }
+    isSecondImage = !isSecondImage;
+    img2.style.opacity = 1;
+  }, 800);
+}
+
+img3.onclick = function () {
+  img3.style.opacity = '5%';
+  setTimeout(function () {
+    if (isSecondImage) {
+      img3.src = 'Pink and Brown Illustrative Happy Birthday A4 Document.png';
+    }
+    else {
+      img3.src = 'f.png';
+    }
+    isSecondImage = !isSecondImage;
+    img3.style.opacity = 1;
+  }, 800);
+
+}
+
+img4.onclick = function () {
+  img4.style.opacity = '5%';
+  setTimeout(function () {
+    if (isSecondImage) {
+      img4.src = 'f.png';
+    }
+    else {
+      img4.src = 'dd.png';
+    }
+    isSecondImage = !isSecondImage;
+    img4.style.opacity = 1;
+  }, 800);
+  
 }
